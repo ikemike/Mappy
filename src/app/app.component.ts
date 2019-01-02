5f8507d1c341d32b9c60115a81165c7b05d342a6
Lemke <Isaac.Lemke@covance.com>
2019-01-02 16:31:25 -0600

    Compare Feature in Progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,19 +27,14 @@ export class AppComponent implements OnInit {
     // @ts-ignore (files isn't recognized)
     let csvFile = document.getElementById("csvInputFileElement").files[0];
     if (csvFile == null) return;
-    let csvData = [];
-    let csvHeaderFields = [];
 
-    console.log('Uploading File...');
+    let csvData = [];
+    this.csvHeaderFields = this.getCSVHeaderRow(csvFile);
 
     this.papa.parse(csvFile, {
       download: true,
+      header: true,
 	    complete: function(results) {
-
-        // Save CSV Header Information
-        results.data[0].forEach(aColumnHeader => {
-          csvHeaderFields.push({selected: false, fieldName: aColumnHeader});
-        })
 
         results.data.forEach(aResult => {
           csvData.push(aResult);
@@ -48,28 +43,74 @@ export class AppComponent implements OnInit {
       }
     });
     this.csvData = csvData;
-    this.csvHeaderFields = csvHeaderFields;
+  }
+
+  // Get each header row in the CSV document 
+  public getCSVHeaderRow(csvFile) {
+    let csvHeaderFields = [];
+    this.papa.parse(csvFile, {
+      download: true,
+	    complete: results => {
+        results.data[0].forEach(aColumnHeader => {
+          csvHeaderFields.push({ selected: false, fieldName: aColumnHeader });
+        })
+      }
+    });
+    return csvHeaderFields;
   }
 
   public retrieveSalesforceContacts() {
+    this.salesforceContacts = [];
+    let baseurl = 'https://cs47.salesforce.com/services/data/v43.0/query/?q=';
+    let query = 'SELECT+Name+,Email+FROM+Contact+LIMIT+3000';
 
-    this.sfService.doSalesforceRestCallout('SELECT+Name+,Email+FROM+Contact+LIMIT+5').then(results => {
-      this.salesforceContacts = JSON.parse(results)["records"];
-      this.salesforceContacts.forEach(aContact => {
-        console.log(aContact);
-      })
+    this.sfService.doSalesforceRestCallout('SELECT+Name+,Email+FROM+Contact+LIMIT+3000').then(results => {
+      let records = JSON.parse(results)["records"];
+      records.forEach(aRecord => { this.salesforceContacts.push(aRecord)})
+
+
+      let nextRecordUrl = JSON.parse(results)["nextRecordsUrl"];
+
+
+      console.log(this.salesforceContacts.length + ' Contacts Retrieved');
+      console.log(nextRecordUrl);
     });
+
+    // Version 2.0 Attempt to use the "nextRecordsUrl" property
+
     
   }
 
+  // UI interaction for showing which CSV fields are currently selected
   public selectCSVColumn(selectedField: CSVHeader) {
     selectedField.selected = selectedField.selected ? false : true;
   }
 
+  // Read-Only property for showing which CSV fields are currently selected 
   get selectedColumns() {
     return this.csvHeaderFields.filter(aField => {
       return aField.selected == true;
     })
+  }
+
+  //Method uses the provided CSV document to attempt to find duplicates 
+  public findContacts() {
+    let matchedRecords = [];
+
+    for (let i = 0; i < this.salesforceContacts.length; i++) {
+      let sfContact = this.salesforceContacts[i];
+      for (let j = 0; j < this.csvData.length; j++) {
+        let csvContact = this.csvData[j];
+        if (sfContact.Email === csvContact.Email && csvContact.Email != '') {
+          matchedRecords.push(sfContact);
+        }
+      }
+    }
+
+    
+    
+    console.log(matchedRecords);
+
   }
 
 }
